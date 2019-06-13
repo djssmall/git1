ade37f423bfb978878e3f48d50264e36488c78d8
small <1342992171@qq.com>
2019-06-13 14:43:57 +0800

    Initial commit

---
+++ src/main/java/com/qfedu/app/App.java
@@ -0,0 +1,12 @@
+package com.qfedu.app;
+
+/**
+ * Created by small on 2019/6/13.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("fscxz");
+        System.out.println("gfdsf");
+    }
+}
